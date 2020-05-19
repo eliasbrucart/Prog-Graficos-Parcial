3ddf36bad56ecdb87cbb9e66dd7b8d0b256a1904
Elias <eliasbrucart@hardgames.com.ar>
2020-05-19 10:01:52 -0300

    Added library code

---
+++ ejercicio1/ejer1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "ejer1.h"
+
+using namespace std;
+
+namespace parcial
+{
+	void mostrarCreditos()
+	{
+		cout << "Elias Brucart, Visual Studio 2017" << endl;
+	}
+
+	float diferencia(float a, float b)
+	{
+		return a - b;
+	}
+}
---
+++ ejercicio1/ejer1.h
@@ -0,0 +1,10 @@
+#ifndef EJER1
+#define EJER1
+
+namespace parcial
+{
+	void mostrarCreditos();
+	float diferencia(float a, float b);
+}
+
+#endif
